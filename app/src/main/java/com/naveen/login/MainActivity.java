4f796f23df67d895bf19504122496c90410a9826
Naveen <naveen.feedback@gmail.com>
2016-08-02 20:45:29 +0530

    Initial commit

---
+++ app/src/main/java/com/naveen/login/MainActivity.java
@@ -0,0 +1,20 @@
+package com.naveen.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = (View) findViewById(R.layout.activity_main);
+    }
+
+
+}
